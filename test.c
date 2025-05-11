ef1cecd0dfea5e65b55831e7b6cee34f7dba6fa5
saiber-elite <jalilotao@gmail.com>
2025-05-11 01:57:27 +0100

    update the theme

---
+++ test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+
+  int t[10,5,8,6,3];
+  
+  for(int i=0, i<5,i++){
+
+    printf("\n\tthe adress of %d is :  %p",i+1,t[i]);
+
+  }
+
+    printf("\n\t\t\t END ");
+    
+  return 0;
+}
